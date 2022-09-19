829030de8681723a81defed5400c20580e7663e6
Horacaz <xistir97@gmail.com>
2022-09-19 17:27:58 -0300

    adds index test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,12 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-/// <reference types= 'Jest'/>
-import initApp from '../index.js';
+/// <reference types= '@types/jest' />
+import initApp from '../app.js';
+import '../index.js';
 
-jest.mock('./index.js', () => jest.fn());
+jest.mock('../app.js', () => jest.fn());
 
 test('Initializes PokeApp', () => {
-  initApp();
   expect(initApp).toHaveBeenCalledTimes(1);
 });
