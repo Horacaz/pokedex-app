7f2c4c74af20f0a5824aecb03fad12257670714a
Horacaz <xistir97@gmail.com>
2022-09-20 16:29:34 -0300

    adds entities and mappers modules

--- src/entities/pokemon.js
+++ src/entities/pokemon.js
@@ -1,5 +1,12 @@
 export default class Pokemon {
-  constructor(pokemon) {
-    this.pokemon = pokemon;
+  constructor(name, id, picture, ability, height, weight, types, stats) {
+    this.name = name;
+    this.id = id;
+    this.picture = picture;
+    this.ability = ability;
+    this.height = height;
+    this.weight = weight;
+    this.types = types;
+    this.stats = stats;
   }
 }
--- src/mappers/pokemon.js
+++ src/mappers/pokemon.js
@@ -1,10 +1,16 @@
-import Pokemon from "../entities/pokemon.js"
+import Pokemon from '../entities/pokemon.js';
 
-export default function mapPokemon(pokemonData){
-const {
-    name: pokemonData,
-} = pokemonData;
-return new Pokemon(
-    pokemonData
-)
+export default function mapPokemon(apiData) {
+  const {
+    name,
+    id,
+    sprites: { other: { 'official-artwork': { front_default: picture } } },
+    abilities: { 0: { ability: { name: ability } } },
+    height,
+    weight,
+    types,
+    stats,
+  } = apiData;
+
+  return new Pokemon(name, id, picture, ability, height, weight, types, stats);
 }
